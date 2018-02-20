5ed478296fbb5b0e0f6300b300a5ff84f2a2c6a3
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-02-20 15:35:09 +0100

    tangle to ini parser org file

---
+++ iniparser.py
@@ -0,0 +1,72 @@
+
+import os
+from collections import OrderedDict as ODict
+
+def parse_to_dict(lines):
+    # Initialize a first group to store key value pairs listed before the first group.
+    groups = ODict()
+    stats = ODict()
+    name = '_root'
+    groups[name] = []
+    for line in lines:
+        line = line.strip()
+        # Skip empty lines
+        if line == "":
+            continue
+        # Identiy a header line
+        if line[0] == '[':
+            if line[-1] != ']':
+                raise ValueError('Found a line starting wih "[" but not ending in "]"!')
+            name = line.lstrip('[').rstrip(']')
+            groups[name] = ODict()
+            stats[name] = [0, 0]
+        # Append normal lines to the last group
+        else:
+            parts = line.split()
+            groups[name][parts[0]] = parts[1:]
+            stats[name] += [0]*(len(parts) - len(stats[name]))
+            for n,p in enumerate(parts):
+                stats[name][n] = max(stats[name][n], len(p))
+
+    return (groups, stats)
+
+def parse_dict_to_str(dct, stats=None):
+    lines = []
+    for group in dct:
+        # blank line before the group header
+        if len(lines) > 0:
+            lines.append('')
+        # Write the header if group is not the root.
+        if group != '_root':
+            lines.append('[{}]'.format(group))
+            lines.append('')
+        # Write the key value pairs
+        for key in dct[group]:
+            if stats is None:
+                lines.append('{}   {}'.format( key, '   '.join(dct[group][key])  ))
+            else:
+                st = stats[group]
+                line = format(key, '{}s'.format(st[0]))
+                for n, val in enumerate(dct[group][key]):
+                    line += '   ' + format(val, '{}s'.format(st[n+1]))
+                lines.append(line)
+    return lines
+
+class IniParser:
+
+    def __init__(self, inifile):
+        self.inifile = inifile
+        self.parse_file_to_dict()
+        self.make_parameter_lookup_dict()
+
+    def parse_file_to_dict(self):
+        """ Load the ini file into memory. """
+        with open(self.inifile, 'r') as rf:
+            self.cfg_dct, self.stats = parse_to_dict(rf)
+
+    def parse_dict_to_str(self):
+        """ Write the dict into lines. """
+        return parse_dict_to_str(self.cfg_dct, self.stats)
+
+    def make_parameter_lookup_dict(self):
+        self.parameter_lookup_dict = parameter_lookup_dict(self.cfg_dct)
