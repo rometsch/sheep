a9de596090bce1057dcb585df3831e10d7e84b25
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-03-28 20:02:09 +0200

    add a setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(  name="sheep"
+        ,version="0.1"
+        ,description="Simulation HElper for Exploring Parameter space."
+        ,author="Thomas Rometsch"
+		,packages=['sheep']
+        )
